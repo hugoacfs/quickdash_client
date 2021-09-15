81b920383969e0535758d940c5a64b6db609c523
Hugo Soares <hugoacfs@gmail.com>
2021-09-15 14:29:57 +0100

    Removing positioned links from here

--- lib.php
+++ lib.php
@@ -8,18 +8,11 @@ function renderdash() {
     $json = file_get_contents($CFG->apipath);
     $links = (array) json_decode($json);
 
-    $positionedlinks = [];
     $arraylinks = [];
     foreach ($links as $link) {
         $link = (array) $link;
-        if (isset($link['position'])) {
-            $positionedlinks[] = $link;
-            continue;
-        }
         $arraylinks[] = (array) $link;
     }
-
-    // $positionedlinks
 
     $dash = $MST->render('dash', ['links' => $arraylinks]);
     return $dash;
